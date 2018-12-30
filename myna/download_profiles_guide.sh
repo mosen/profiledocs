64f481270ec3a4047f89126f8dd15c6071658a60
Mosen <mosen@users.noreply.github.com>
2018-12-30 23:28:26 +1100

    Fix PDF download script

--- myna/download_profiles_guide.sh
+++ myna/download_profiles_guide.sh
@@ -2,13 +2,13 @@
 
 if [[ ! -f ./Configuration-Profile-Reference.pdf ]]; then
     echo "No configuration profile reference found, downloading..."
-    curl -O https://developer.apple.com/enterprise/documentation/Configuration-Profile-Reference.pdf
+    curl -O https://developer.apple.com/business/documentation/Configuration-Profile-Reference.pdf
 fi
 
 
 if [[ ! -f ./MDM-Protocol-Reference.pdf ]]; then
     echo "No MDM protocol reference found, downloading..."
-    curl -O https://developer.apple.com/enterprise/documentation/MDM-Protocol-Reference.pdf
+    curl -O https://developer.apple.com/business/documentation/MDM-Protocol-Reference.pdf
 fi
 
 
